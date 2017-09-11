0140dc3a1c90c5672aa1089fe09974ba9ba5e896
Tony Nguyen <ngn.tony@gmail.com>
2017-09-12 00:52:01 +0200

    test sequelize

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const Telegraf = require('telegraf')
+const Sequelize = require('sequelize');
 
 const app = new Telegraf(process.env.BOT_TOKEN)
+const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 app.command('start', ({ from, reply }) => {
- console.log('start', from)
- return reply('Welcome!')
+    console.log('start', from)
+    return reply('Welcome!')
 })
 
 app.hears('hi', (ctx) => ctx.reply('Hey there!'))
 app.on('sticker', (ctx) => ctx.reply('👍'))
 app.hears('pikachu', (ctx) => ctx.reply('Wesh!!'))
+app.hears('testdb', (ctx) => 
+{
+    sequelize
+        .authenticate()
+        .then(() => {
+            ctx.reply('Connection has been established successfully.');
+        })
+        .catch(err => {
+            ctx.reply('Unable to connect to the database:', err);
+        });
+})
 app.startPolling()
